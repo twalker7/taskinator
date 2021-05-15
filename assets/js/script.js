6329104e706523f4173a4ef27e14cc84b7008e4f
Thomas Walker <thomasnwalker@yahoo.com>
2021-05-15 10:59:59 -0700

    preventDefault added and eventlistener scope broadened to whole form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
+//function for creating new <li> and appending them, styled, to the ul tasksToDoEl
 var createTaskHandler = function(){
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "this is a new task";
     tasksToDoEl.appendChild(listItemEl);
 
 }
-
-buttonEl.addEventListener("click", createTaskHandler);
+//clicking submit or hitting enter calls the createTaskHandler function 
+formEl.addEventListener("submit", createTaskHandler);
 
